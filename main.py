fbc7c3c5138dc40f8d31c982971f14e31c3a0df4
kev1n999 <sopratesta999@gmail.com>
2026-02-06 19:32:58 -0300

    add function to fetch and type words

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
+import pyautogui
 from selenium import webdriver
-from config.constants import URL 
-from typing import List
+from config.constants import URL
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 test_routes = {
   1:"/1-minute",
@@ -16,22 +18,41 @@ test_routes = {
 
 def open(driver: webdriver.Chrome, url: str) -> None:
   driver.get(url)
+  assert "minute" in url or "page" in url, "Invalid page!"
   assert "typing" in driver.title.lower(), "An error ocurred to open!"
 
-def characters_parser(driver: webdriver.Chrome) -> List[str]:
+def fetch_words(driver: webdriver.Chrome) -> None:
+  class_name = "screenBasic-word"
+  current_word = ""
+
   try:
     content_container = WebDriverWait(driver, 10).until(
-      EC.presence_of_element_located((By.CLASS_NAME, "green-card")) 
+      EC.presence_of_all_elements_located((By.CLASS_NAME, class_name))
     )
+  except TimeoutException as timeout_err:
+    raise TimeoutException(f"content_container not found!\n{timeout_err}")
 
-    content = content_container.text
-    print(content)
-  except:
-    raise Exception("Content container not found!")
+  for word in content_container:
+    current_word += word.text
+
+  for _ in range(len(current_word)):
+    try:
+      active = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.CSS_SELECTOR, ".letter.is-active"))
+
+      letter = active.text
+      if letter.isspace():
+        pyautogui.press(["space"])
+        time.sleep(1)
+        # A bug after that (The first letter of the next word doesn't works)
+
+      pyautogui.press(letter)
+      print(letter, end=" ")
+    except Exception as err:
+      print(f"An error ocurred to try typing: {err}")
 
 def get_test_route() -> str:
   print("[1] => 1 minute\n[2] 3 minutes\n[3] 5 minutes")
-  print("[4] => 1 page\n[5] => 2 pages\n[6] => 3 pages") 
+  print("[4] => 1 page\n[5] => 2 pages\n[6] => 3 pages")
 
   option = int(input("\n[choice a option] _: "))
 
@@ -46,13 +67,12 @@ def start_test(driver: webdriver.Chrome) -> None:
     if EC.element_to_be_clickable(continue_button):
       continue_button.click()
       print("You're in the test!")
-
   except:
     raise Exception("An error ocurred to start the test!")
-  
+
 if __name__ == "__main__":
   test_route = get_test_route()
   driver = webdriver.Chrome()
   open(driver, URL + test_route)
   start_test(driver)
-  characters_parser(driver)
+  print(fetch_words(driver))
